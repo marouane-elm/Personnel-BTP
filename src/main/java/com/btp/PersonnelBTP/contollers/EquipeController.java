6527c7b36a68b7643f284374e6c60c872e49d577
Marouane ELM <marouane.elmhhoujab11@gmail.com>
2021-03-12 16:05:20 +0100

    test commit

--- src/main/java/com/btp/PersonnelBTP/contollers/EquipeController.java
+++ src/main/java/com/btp/PersonnelBTP/contollers/EquipeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/equipes")
 public class EquipeController {
-
+// llll
     @Autowired
     EquipeService equipeService;
 
